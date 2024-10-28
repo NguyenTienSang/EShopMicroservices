fb811826d671438891abb73586cea33c189d285c
NguyenTienSang <tiensang07@gmail.com>
2024-10-28 14:23:54 +0700

    AddExceptionHandling

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Desciption, string ImageFile, decimal Price) 
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+        RuleFor(x => x.Price).NotEmpty().WithMessage("Price must be greater than 0");
+    }
+}
+
+internal class CreateProductCommandHandler 
+    (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Create Product entity from command object
+
+        //Save to database
+
+        //Result CrateProductResult result
+
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Desciption,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //Save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        //return result
+        return new CreateProductResult(product.Id);
+
+        
+    }
+}
