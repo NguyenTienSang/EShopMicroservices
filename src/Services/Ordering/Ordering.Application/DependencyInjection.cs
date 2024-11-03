962883b8bb562ae02e6e9b659250f1c973dc90d7
NguyenTienSang <tiensang07@gmail.com>
2024-11-03 13:56:56 +0700

    Ordering.Infrastructure Layer and SQL Server

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application;
 
@@ -6,6 +9,15 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        //services.AddFeatureManagement();
+        //services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
         return services;
     }
 }
